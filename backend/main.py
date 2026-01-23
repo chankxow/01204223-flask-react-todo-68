40b43422a898d4f9e5e7432ed7d59a0aac64767c
chankxow <kittipoomzx555@gmail.com>
2026-01-23 15:12:53 +0700

    add migration file

--- backend/main.py
+++ backend/main.py
@@ -21,14 +21,16 @@ class TodoItem(db.Model):
     title: Mapped[str] = mapped_column(String(100))
     done: Mapped[bool] = mapped_column(default=False)
 
-    ##### เพิ่มส่วน relationship  ซึ่งตรงนี้จะไม่กระทบ schema database เลย (เพราะว่าไม่มีการ map ไปยังคอลัมน์ใดๆ)
     comments: Mapped[list["Comment"]] = relationship(back_populates="todo")
 
     def to_dict(self):
         return {
             "id": self.id,
             "title": self.title,
-            "done": self.done
+            "done": self.done,
+            "comments": [
+                comment.to_dict() for comment in self.comments
+            ]
         }
 
 class Comment(db.Model):
@@ -37,6 +39,13 @@ class Comment(db.Model):
     todo_id: Mapped[int] = mapped_column(ForeignKey('todo_item.id'))
 
     todo: Mapped["TodoItem"] = relationship(back_populates="comments")
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "message": self.message,
+            "todo_id": self.todo_id
+        }
 
 
 INITIAL_TODOS = [
