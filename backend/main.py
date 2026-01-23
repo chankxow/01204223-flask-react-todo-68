985e2eaf710a3835f620c16d4185705a2cef6c9e
chankxow <kittipoomzx555@gmail.com>
2026-01-23 15:09:16 +0700

    add migrate

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,8 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Integer, String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import Integer, String,ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column,relationship   
 from sqlalchemy.orm import DeclarativeBase
 from flask_migrate import Migrate       
 
@@ -17,10 +17,12 @@ class Base(DeclarativeBase):
 db = SQLAlchemy(app, model_class=Base)
 migrate = Migrate(app, db)        
 class TodoItem(db.Model):
-    
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     title: Mapped[str] = mapped_column(String(100))
     done: Mapped[bool] = mapped_column(default=False)
+
+    ##### เพิ่มส่วน relationship  ซึ่งตรงนี้จะไม่กระทบ schema database เลย (เพราะว่าไม่มีการ map ไปยังคอลัมน์ใดๆ)
+    comments: Mapped[list["Comment"]] = relationship(back_populates="todo")
 
     def to_dict(self):
         return {
@@ -28,6 +30,13 @@ class TodoItem(db.Model):
             "title": self.title,
             "done": self.done
         }
+
+class Comment(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    message: Mapped[str] = mapped_column(String(250))
+    todo_id: Mapped[int] = mapped_column(ForeignKey('todo_item.id'))
+
+    todo: Mapped["TodoItem"] = relationship(back_populates="comments")
 
 
 INITIAL_TODOS = [
